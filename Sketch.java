463b47166b629b83f2fff6a1bcd79c1472d14ee3
SohPar <sohail.parpia23@ycdsbk12.ca>
2022-05-10 08:48:34 -0400

    Processing Task Commit #1

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,22 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
+
+// Local Variables For The Array
+float[] circleY = new float[50];
+float[] snow = new float[50];
+float[] mousePositionsX = new float[25];
+float[] mousePositionsY = new float[25];
+int speed = 4;
+int num = 25;
+int index = 0;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(300, 300);
   }
 
   /** 
@@ -16,21 +24,57 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    for (int i = 0; i < 50; i++) {
+      circleY[i] = random(height);
+    }
+    for (int i = 0; i < 50; i++) {
+      snow[i] = 5;
+    }
   }
+    
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
+    background(19, 24, 98);
+
+ for (int circleX = 0; circleX < 50; circleX++) {
+  ellipse(circleX*8, circleY[circleX], 15, 15);
 }
+
+for (int i = 0; i < 50; i++) {
+  circleY[i] += speed;
+  if (circleY[i] >= 400) {
+    circleY[i] = 0;
+    snow[i] += 5;
+  }
+}
+
+for (int i = 0; i < 50; i++) {
+  fill(255, 255, 255);
+  rect(i*8, 400-snow[i], 8, 400);
+}
+
+// Moving the object around the snowfall with mouse
+mousePositionsX[index] = mouseX;
+mousePositionsY[index] = mouseY;
+index = (index + 1) % num;
+for (int i = 0; i < 25; i++) {
+  int pos = (index + i) % num;
+  ellipse(mousePositionsX[pos], mousePositionsY[pos], i, i);
+}
+}
+
+  // Control Speed of The SnowFall
+  public void keyPressed() {
+    if (keyCode == UP) {
+      speed = 5;
+    }
+    if (keyCode == DOWN) {
+      speed = 2;
+    }
+  }
+}
+
