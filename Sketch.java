d0a80a18caab08f6d4e65c12c593925e187b7ad0
SohPar <sohail.parpia23@ycdsbk12.ca>
2022-05-12 19:08:30 -0400

    New Updated Code

--- Sketch.java
+++ Sketch.java
@@ -2,11 +2,14 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-// Local Variables For The Array
+// Local Variables For The Snowfall
 float[] circleY = new float[50];
 float[] snow = new float[50];
 float[] mousePositionsX = new float[25];
 float[] mousePositionsY = new float[25];
+int playerX = 0;
+int playerY = 0;
+int playerSpeed = 3;
 int speed = 3;
 int num = 25;
 int index = 0;
@@ -16,7 +19,7 @@ int index = 0;
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(350, 350);
   }
 
   /** 
@@ -32,51 +35,56 @@ int index = 0;
     }
   }
     
-
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
 
-    background(19, 24, 98);
+    background(0);
 
- for (int circleX = 0; circleX < 50; circleX++) {
-  ellipse(circleX*8, circleY[circleX], 20, 20);
-}
+    fill(255);
+    for (int circleX = 0; circleX < 50; circleX++) {
+      ellipse(circleX*8, circleY[circleX], 20, 20);
+    }
 
-for (int i = 0; i < 50; i++) {
-  circleY[i] += speed;
-  if (circleY[i] >= 400) {
-    circleY[i] = 0;
-    snow[i] += 5;
-  }
-}
+    for (int i = 0; i < 50; i++) {
+      circleY[i] += speed;
+      if (circleY[i] >= 400) {
+        circleY[i] = 0;
+        snow[i] += 5;
+      }
+    }
 
-for (int i = 0; i < 50; i++) {
-  fill(255, 255, 255);
-  rect(i*8, 400-snow[i], 8, 400);
-}
+    for (int i = 0; i < 50; i++) {
+      rect(i*8, 400-snow[i], 8, 400);
+    }
 
-// Move the object around the snowfall with mouse
-mousePositionsX[index] = mouseX;
-mousePositionsY[index] = mouseY;
-index = (index + 1) % num;
-for (int i = 0; i < 25; i++) {
-  int pos = (index + i) % num;
-  ellipse(mousePositionsX[pos], mousePositionsY[pos], i, i);
-  speed = 1;
+    fill(15, 215, 250);
+    ellipse(playerX, playerY, 20, 20);
 
 }
-}
 
-  // Control Speed of The SnowFall
+  // Control Speed of SnowFall + Move the ball around the snowfall
   public void keyPressed() {
-    if (keyCode == DOWN) {
-      speed = 5;
-    }
-    if (keyCode == UP) {
+  if (keyCode == DOWN) {
+      speed = 2;
+  }
+  if (keyCode == UP) {
       speed = 1;
-    }
+  }
+  if (key == 'w') {
+    playerY -= playerSpeed;
+  }
+  if (key == 'a') {
+    playerX -= playerSpeed;
+  }
+  if (key == 's') {
+    playerY += playerSpeed;
+  }
+  if (key == 'd') {
+    playerX += playerSpeed;
   }
 }
+}
+
 
